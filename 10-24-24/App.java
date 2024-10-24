92d092074551f134d837d7e8e5f07ca3b98b38f2
Pedro Sodre <pedro.malini@edu.ufes.br>
2024-10-24 14:55:41 -0300

    v.0.2

--- 10-24-24/App.java
+++ 10-24-24/App.java
@@ -1,29 +1,39 @@
-class Time
-{
-    private String nome;
-    private int nMundiais;
-    public void setNome(String nome)
-    {
-        this.nome = nome;
-    }
-    public String getNome()
-    {
-        return this.nome;
-    }
-    public void incnMundiais()
-    {
-        this.nMundiais++;
-    }
-    public int getnMundiais()
-    {
-        return this.nMundiais;
+import java.util.*;
+
+public class App{
+    public static void main(String[] args){
+        // sem importar lista:   java.util.LinkedList<Partida> partidas = new java.util.LinkedList<Partida>();
+        LinkedList<Partida> partidas = new LinkedList<Partida>();
+
+        Time flamengo = new Time("Flamengo");
+        Time vasco = new Time("Vasco");
+
+        Partida p = new Partida(flamengo, vasco);
+        Partida p2 = new Partida(flamengo, vasco);
+        partidas.add(p);
+        partidas.add(p2);
+        System.out.println(partidas.size());
+
+        for(Partida j : partidas)
+        {
+            System.out.print(j.getTimeA().getNome());
+            System.out.print(" x ");
+            System.out.print(j.getTimeB().getNome());
+            System.out.print("\n");
+        }
+
+        flamengo.incnMundiais();
+        System.out.println("O " + flamengo.getNome() + " tem " + flamengo.getnMundiais() + " mundial");
     }
 }
 
 
-public class App{
-    public static void main(String[] args){
-        Time flamengo = new Time();
+/*
+
+SEI LA UNS CODIGOS ALEATORIOS QUE TEVE NA AULA
+
+        Time flamengo = new Time("Mengoooooooooo");
+        System.out.println(flamengo.getNome());
         flamengo.setNome("Flamengo");
         System.out.println(flamengo.getNome());
         flamengo.setNome("Vascaoooooooooooo");
@@ -31,12 +41,10 @@ public class App{
         System.out.println(flamengo.getnMundiais());
         flamengo.incnMundiais();
         System.out.println(flamengo.getnMundiais());
-    }
-}
-
+ */
 
 /*
-System.out.println("Hello!");
+        System.out.println("Hello!");
         if(args.length >= 1)
         {
             System.out.println("Boa cria");
